9959e84e252913c53b0a1ba49d00084251d371ed
Alberto <toote_pr@hotmail.com>
2021-11-17 20:48:38 -0600

    Validacion registrar medico

--- Modulos/registrarMedicos.py
+++ Modulos/registrarMedicos.py
@@ -13,9 +13,113 @@ class WindowTwo(QtWidgets.QMainWindow):
         self.setWindowTitle("J-Medic: Registrar Medicos")
         self.stylesheet = self.estilos()
         self.setStyleSheet(self.stylesheet)
-        self.botonGuardar.clicked.connect(self.switch)
+        self.botonGuardar.clicked.connect(self.validar_datos)
         self.actionRegresar.setShortcut("Ctrl+R")
         self.actionRegresar.triggered.connect(self.switch)
+        self.validar()
+
+    def validar(self):
+        self.inputNombre.textChanged.connect(self.validar_nombre)
+        self.inputAp.textChanged.connect(self.validar_apellidoP)
+        self.inputAm.textChanged.connect(self.validar_apellidoM)
+        self.inputTelefono.textChanged.connect(self.validar_telefono)
+        self.inputCedula.textChanged.connect(self.validar_cedula)
+        self.inputContra.textChanged.connect(self.validar_password)
+        pass
+
+    def validar_nombre(self):
+        nombre = self.inputNombre.text()
+        validar = re.match(
+            "^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{1,}$", nombre, re.I)
+        if nombre == "":
+            self.inputNombre.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.inputNombre.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.inputNombre.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_apellidoP(self):
+        apellido = self.inputAp.text()
+        validar = re.match(
+            "^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{1,}$", apellido, re.I)
+        if apellido == "":
+            self.inputAp.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.inputAp.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.inputAp.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_apellidoM(self):
+        apellido = self.inputAm.text()
+        validar = re.match(
+            "^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{1,}$", apellido, re.I)
+        if apellido == "":
+            self.inputAm.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.inputAm.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.inputAm.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_cedula(self):
+        cedula = self.inputCedula.text()
+        validar = re.match("^\d{10}$", cedula, re.I)
+        if cedula == "":
+            self.inputCedula.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.inputCedula.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.inputCedula.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_telefono(self):
+        telefono = self.inputTelefono.text()
+        validar = re.match("^\d{10}$", telefono, re.I)
+        if telefono == "":
+            self.inputTelefono.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.inputTelefono.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.inputTelefono.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_turno(self):
+        turno = self.inputTurno.currentText()
+        if turno == "":
+            return False
+        else:
+            return True
+
+    def validar_password(self):
+        password = self.inputUsuario.text()
+        password2 = self.inputContra.text()
+        if password != password2:
+            self.inputContra.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.inputContra.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_datos(self):
+        if self.validar_nombre() and self.validar_cedula() and self.validar_apellidoP() and self.validar_turno() and self.validar_apellidoM() and self.validar_telefono() and self.validar_password():
+            QMessageBox.information(
+                    self, "Datos guardados", "Su informacion se ha guardado correctamente", QMessageBox.Discard)
+            self.switch()
+        else:
+           QMessageBox.warning(
+                self, "Error", "Ingresa los datos correctamente", QMessageBox.Discard)
 
     def estilos(self):
         estilo = """
