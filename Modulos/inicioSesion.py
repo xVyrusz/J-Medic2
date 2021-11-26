4243deee5d252c3027a402826b72b209fd212255
Erasmo Carreon <c4rreon1234@gmail.com>
2021-11-26 04:07:14 -0600

    Face Id hecho

--- Modulos/inicioSesion.py
+++ Modulos/inicioSesion.py
@@ -1,6 +1,8 @@
 import bcrypt
 from PyQt5 import uic, QtCore, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
+import cv2
+import os
 
 
 class Login(QtWidgets.QMainWindow):
@@ -14,6 +16,7 @@ class Login(QtWidgets.QMainWindow):
         self.stylesheet = self.estilos()
         self.setStyleSheet(self.stylesheet)
         self.pushButton.clicked.connect(self.login)
+        self.pushButton_2.clicked.connect(self.login2)
 
     def estilos(self):
         estilo = """
@@ -63,4 +66,48 @@ class Login(QtWidgets.QMainWindow):
         return estilo
 
     def login(self):
+        self.switch_window.emit()
+
+    def login2(self):
+        dataPath = "./Data"
+        imagePaths = os.listdir(dataPath)
+
+        face_recognizer = cv2.face.EigenFaceRecognizer_create()
+
+        # Leyendo el modelo
+        face_recognizer.read("modeloEigenFaces.xml")
+
+        cap = cv2.VideoCapture(1)
+
+        faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
+        salir = 0
+
+        while True:
+            ret, frame = cap.read()
+            if ret == False: break
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            auxFrame = gray.copy()
+
+            faces = faceClassif.detectMultiScale(gray,1.3,5)
+
+            for (x,y,w,h) in faces:
+                rostro = auxFrame[y:y+h,x:x+w]
+                rostro = cv2.resize(rostro,(150,150), interpolation= cv2.INTER_CUBIC)
+                result = face_recognizer.predict(rostro)
+
+                cv2.putText(frame, "{}".format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.cv2.LINE_AA)
+                if result[1] < 5700:
+                    cv2.putText(frame,"{}".format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+                    cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+                    salir=1
+                else:
+                    cv2.putText(frame,"Desconocido",(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+                    cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+
+            cv2.imshow("frame",frame)
+            k =cv2.waitKey(1)
+            if k == 27 or salir == 1:
+                break
+        cap.release()
+        cv2.destroyAllWindows()
         self.switch_window.emit()
