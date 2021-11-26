93c0c3d8e21cac27cd9238f1765a21e25a27f844
Erasmo Carreon <c4rreon1234@gmail.com>
2021-11-26 05:04:02 -0600

    Añadido registrar cara

--- Modulos/registrarMedicos.py
+++ Modulos/registrarMedicos.py
@@ -1,7 +1,10 @@
 import re
 from PyQt5 import uic, QtCore, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
-
+import cv2
+import os
+import numpy as np
+import imutils
 
 class WindowTwo(QtWidgets.QMainWindow):
 
@@ -14,6 +17,7 @@ class WindowTwo(QtWidgets.QMainWindow):
         self.stylesheet = self.estilos()
         self.setStyleSheet(self.stylesheet)
         self.botonGuardar.clicked.connect(self.validar_datos)
+        self.botonRostro.clicked.connect(self.validar_datos2)
         self.actionRegresar.setShortcut("Ctrl+R")
         self.actionRegresar.triggered.connect(self.switch)
         self.validar()
@@ -197,6 +201,73 @@ class WindowTwo(QtWidgets.QMainWindow):
 
         """
         return estilo
+
+    def validar_datos2(self):
+        if self.validar_cedula():
+
+            personName = self.inputCedula.text()
+            dataPath = "./Data"
+            personPath = dataPath + "/" + personName
+
+            if not os.path.exists(personPath):
+                os.makedirs(personPath)
+
+            cap = cv2.VideoCapture(1)
+
+            faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
+            count = 0
+
+            while True:
+                ret, frame = cap.read()
+                if ret == False: break
+                frame = imutils.resize(frame, width=640)
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                auxFrame = frame.copy()
+
+                faces = faceClassif.detectMultiScale(gray,1.3,5)
+
+                for (x,y,w,h) in faces:
+                    cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0),2)
+                    rostro = auxFrame[y:y+h, x:x+w]
+                    rostro = cv2.resize(rostro, (150,150), interpolation=cv2.INTER_CUBIC)
+                    cv2.imwrite(personPath + "/rostro_{}.jpg".format(count),rostro)
+                    count = count + 1
+                cv2.imshow("frame",frame)
+
+                k = cv2.waitKey(1)
+                if k == 27 or count >= 300:
+                    break
+
+            cap.release()
+            cv2.destroyAllWindows()
+
+            peopleList = os.listdir(dataPath)
+
+            labels = []
+            facesData = []
+            label = 0
+
+            for nameDir in peopleList:
+                personpath = dataPath + "/" + nameDir
+
+                for fileName in os.listdir(personpath):
+                    labels.append(label)
+                    facesData.append(cv2.imread(personpath+"/"+fileName,0))
+                    image = cv2.imread(personpath+"/"+fileName,0)
+                label = label + 1
+
+            face_recognizer = cv2.face.EigenFaceRecognizer_create()
+
+            #Entrenando el reconocedor de rostros
+            face_recognizer.train(facesData, np.array(labels))
+
+            #Almacendnado el modelo obtenido
+            face_recognizer.write("modeloEigenFaces.xml")
+            QMessageBox.information(
+                    self, "Cara guardada", "Su informacion se ha guardado correctamente", QMessageBox.Discard)
+        else:
+           QMessageBox.warning(
+                self, "Error", "Ingresa los datos correctamente", QMessageBox.Discard)
 
     def switch(self):
         self.switch_window.emit()
