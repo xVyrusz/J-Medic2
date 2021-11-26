4982945c47bc6c8365aa92ffc3578708a358f075
Erasmo Carreon <c4rreon1234@gmail.com>
2021-11-26 08:41:00 -0600

    validar buscar paciente

--- Modulos/buscarPaciente.py
+++ Modulos/buscarPaciente.py
@@ -13,11 +13,86 @@ class WindowTwo(QtWidgets.QMainWindow):
         self.stylesheet = self.estilos()
         self.setStyleSheet(self.stylesheet)
         self.botonBuscar.clicked.connect(self.switch)
-        self.buscarId.clicked.connect(self.switch)
-        self.buscarNombre.clicked.connect(self.switch)
-        self.buscarApellidoP.clicked.connect(self.switch)
+        self.buscarId.clicked.connect(self.validar_datos)
+        self.buscarNombre.clicked.connect(self.validar_datos_2)
+        self.buscarApellidoP.clicked.connect(self.validar_datos_3)
         self.actionRegresar.setShortcut("Ctrl+R")
         self.actionRegresar.triggered.connect(self.switch)
+        self.validar()
+
+    def validar(self):
+        self.inputIdPaciente.textChanged.connect(self.validar_id_paciente)
+        self.inputNombre.textChanged.connect(self.validar_nombre)
+        self.inputAp.textChanged.connect(self.validar_apellidoP)
+        pass
+
+    def validar_id_paciente(self):
+        id_paciente = self.inputIdPaciente.text()
+        validar = re.match("^\d{1,}$", id_paciente, re.I)
+        if id_paciente == "":
+            self.inputIdPaciente.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.inputIdPaciente.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.inputIdPaciente.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_datos(self):
+        if self.validar_id_paciente():
+            #aqui va la consulta
+            QMessageBox.information(
+                self,"Correcto", "Busqueda exitosa", QMessageBox.Discard)
+        else:
+           QMessageBox.warning(
+                self, "Error", "Ingresa los datos correctamente", QMessageBox.Discard)
+
+    def validar_nombre(self):
+        nombre = self.inputNombre.text()
+        validar = re.match(
+            "^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{1,}$", nombre, re.I)
+        if nombre == "":
+            self.inputNombre.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.inputNombre.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.inputNombre.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_datos_2(self):
+        if self.validar_nombre():
+            #aqui va la consulta
+            QMessageBox.information(
+                self, "Correcto","Busqueda exitosa", QMessageBox.Discard)
+        else:
+           QMessageBox.warning(
+                self, "Error", "Ingresa los datos correctamente", QMessageBox.Discard)
+
+    def validar_apellidoP(self):
+        apellido = self.inputAp.text()
+        validar = re.match(
+            "^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{1,}$", apellido, re.I)
+        if apellido == "":
+            self.inputAp.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.inputAp.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.inputAp.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_datos_3(self):
+        if self.validar_apellidoP():
+            #aqui va la consulta
+            QMessageBox.information(
+                self, "Correcto","Busqueda exitosa", QMessageBox.Discard)
+        else:
+           QMessageBox.warning(
+                self, "Error", "Ingresa los datos correctamente", QMessageBox.Discard)
 
     def estilos(self):
         estilo = """
