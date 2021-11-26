eaad0a0c346e7ddd8f3c83a66ed8ba7bbb22e48a
Erasmo Carreon <c4rreon1234@gmail.com>
2021-11-26 08:15:46 -0600

    validacion citas

--- Modulos/registrarCita.py
+++ Modulos/registrarCita.py
@@ -12,9 +12,67 @@ class WindowTwo(QtWidgets.QMainWindow):
         self.setWindowTitle("J-Medic: Registrar Cita")
         self.stylesheet = self.estilos()
         self.setStyleSheet(self.stylesheet)
-        self.agendarCita.clicked.connect(self.switch)
+        self.agendarCita.clicked.connect(self.validar_datos)
         self.actionRegresar.setShortcut("Ctrl+R")
         self.actionRegresar.triggered.connect(self.switch)
+        self.validar()
+
+    def validar(self):
+        self.inputIdPaciente.textChanged.connect(self.validar_id_paciente)
+        #self.input_fecha.textChanged.connect(self.validar_fecha)
+        pass
+
+    def validar_id_paciente(self):
+        id_paciente = self.inputIdPaciente.text()
+        validar = re.match("^\d{1,}$", id_paciente, re.I)
+        if id_paciente == "":
+            self.inputIdPaciente.setStyleSheet("border: 2px solid yellow;")
+            return False
+        elif not validar:
+            self.inputIdPaciente.setStyleSheet("border: 2px solid red;")
+            return False
+        else:
+            self.inputIdPaciente.setStyleSheet("border: 2px solid green;")
+            return True
+
+    def validar_fecha(self):
+        anio = self.inputA.currentText()
+        mes = self.inputM.currentText()
+        dia = self.inputD.currentText()
+        hora = self.inputH.currentText()
+        minutos = self.inputMin.currentText()
+        cont = 0
+        if anio == "":
+            cont+=1
+
+        if mes == "":
+            cont+=1
+
+        if dia == "":
+            cont+=1
+
+        if hora == "":
+            cont+=1
+
+        if minutos == "":
+            cont+=1
+
+        fecha=str(anio+mes+dia+hora+minutos)
+        if cont>0:
+            fecha=""
+        
+        if fecha == "":
+            return False
+        else:
+            return True
+
+    def validar_datos(self):
+        if self.validar_id_paciente() and self.validar_fecha():
+            QMessageBox.information(
+                    self, "Datos guardados", "Su cita se genero correctamente", QMessageBox.Discard)
+            self.switch()
+        else:
+            QMessageBox.warning(self, "Error", "Ingresa los datos correctamente", QMessageBox.Discard)
 
     def estilos(self):
         estilo = """
